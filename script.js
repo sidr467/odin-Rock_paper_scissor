ad48c633626c1c5cf9aeac234f88199c009e20e6
sidr467 <146876430+sidr467@users.noreply.github.com>
2023-12-07 00:31:32 +0530

    and also playRound function to decide who won

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function getComputerChoice(){
 
 getComputerChoice();
 
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return "The game is tie";
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        return "you loose paper beats rock";
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "you win paper beats rock";
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "you win scissors beats paper";
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return "you loose scissors beats paper";
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return "you win rock beats scissors";
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return "you loose rock beats scissors";
+    }
+}
+
+playerSelection = "scissors";
+computerSelection = getComputerChoice().toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
